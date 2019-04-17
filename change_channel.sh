173d9a50ff0bae0f935a015a03641effdda14d97
landry.matthews@gmail.com <landry.matthews@gmail.com>
2019-04-17 01:59:27 +0000

    msh

--- change_channel.sh
+++ change_channel.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-echo switching to channel $1 
-sudo sed -i "/channel=/c\channel=$1" /etc/hostapd/hostapd.conf
-sudo hostapd_cli disable
-sudo hostapd_cli set channel $1
-sudo hostapd_cli enable
+
+source /etc/hostapd/hostapd.conf
+echo $channel
+echo above is my channel
+if [ $channel -eq $1 ] ; then
+	echo Already on the BEST channel
+else
+	echo switching to channel $1 
+	sudo sed -i "/channel=/c\channel=$1" /etc/hostapd/hostapd.conf
+	sudo hostapd_cli disable
+	sudo hostapd_cli set channel $1
+	sudo hostapd_cli enable
+fi
